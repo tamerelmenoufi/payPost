466dd6c393b84d3a71685706f4587bca3df7d388
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-05-29 23:39:38 +0300

    Atualizações

--- lib/fn.php
+++ lib/fn.php
@@ -101,9 +101,9 @@
         );
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 
-        $result = curl_exec($ch);
+        echo $result = curl_exec($ch);
         if (curl_errno($ch)) {
-            // echo 'Error:' . curl_error($ch);
+            echo 'Error:' . curl_error($ch);
         }
         curl_close($ch);
 
