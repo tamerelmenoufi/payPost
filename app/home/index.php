c3ee4311ff375b5f171a2f569b159286aeb87d81
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-05-29 22:24:58 +0300

    Atualizações

--- app/home/index.php
+++ app/home/index.php
@@ -30,7 +30,7 @@
         <div class="row g-0">
             <div class="col">
                 <div class="d-flex justify-content-center  flex-column" >
-                    <div class="card">
+                    <div class="card" style="width:70%">
                         <img src="img/logo.png" class="card-img-top" />
                         <div class="card-body">
                             
