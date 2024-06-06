9a967c5b64074de6b5e7886f555c3ab8ebaf2e4c
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-06-06 19:47:25 +0300

    Atualizações

--- src/dashboard/index.php
+++ src/dashboard/index.php
@@ -90,7 +90,7 @@
                     </thead>
                     <tbody>
                     <?php
-                    $q = "select count(*) as quantidade, (select count(*) from vendas where deletado != '1') as total, b.combustivel, c.nome from vendas a left join combustiveis b on a.combustivel = b.codigo left join usuarios c on a.usuario = c.codigo where a.deletado != '1' group by a.combustivel order by quantidade desc";
+                    $q = "select count(*) as quantidade, (select count(*) from vendas where deletado != '1' {$where}) as total, b.combustivel, c.nome from vendas a left join combustiveis b on a.combustivel = b.codigo left join usuarios c on a.usuario = c.codigo where a.deletado != '1' {$where} group by a.combustivel order by quantidade desc";
                     $r = mysqli_query($con, $q);
                     while($s = mysqli_fetch_object($r)){
 
@@ -133,7 +133,7 @@
                     </thead>
                     <tbody>
                     <?php
-                    $q = "select count(*) as quantidade, (select count(*) from vendas where deletado != '1') as total, b.combustivel, c.nome from vendas a left join combustiveis b on a.combustivel = b.codigo left join usuarios c on a.usuario = c.codigo where a.deletado != '1' group by a.usuario order by quantidade desc";
+                    $q = "select count(*) as quantidade, (select count(*) from vendas where deletado != '1' {$where}) as total, b.combustivel, c.nome from vendas a left join combustiveis b on a.combustivel = b.codigo left join usuarios c on a.usuario = c.codigo where a.deletado != '1' {$where} group by a.usuario order by quantidade desc";
                     $r = mysqli_query($con, $q);
                     while($s = mysqli_fetch_object($r)){
 
