f438bf7fca7a6ca8c3288d13128ba30f9b36ea7f
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-06-06 12:13:32 +0300

    Atualizações

--- src/usuarios/index.php
+++ src/usuarios/index.php
@@ -109,6 +109,8 @@
                 <thead>
                   <tr>
                     <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">E-mail</th>
                     <th scope="col">CPF</th>
                     <th scope="col">Situação</th>
                     <th scope="col">Ações</th>
@@ -123,6 +125,8 @@
                   ?>
                   <tr>
                     <td class="w-100"><?=$d->nome?></td>
+                    <td><?=$d->telefone?></td>
+                    <td><?=$d->email?></td>
                     <td><?=$d->cpf?></td>
                     <td>
 
